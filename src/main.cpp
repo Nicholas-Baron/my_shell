929242e614a6012d7b25a8fd488e2ed00017b019
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-04-22 20:16:55 -0700

    Added a non-interactive mode for executing scripts.

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,51 @@
 #include "parser.h"
 #include "tokenizer.h"
 
+#include <fstream>  // std::ifstream
 #include <iostream> // std::cin
+#include <sstream>  // std::stringstream
 #include <string>
 
-int main() {
+constexpr bool debug = false;
+
+static std::string read_entire_file(std::ifstream && file) {
+
+    std::stringstream stream;
+    std::string buffer;
+    while (std::getline(file, buffer)) { stream << buffer << '\n'; }
+
+    return stream.str();
+}
+
+int main(int arg_count, const char ** args) {
+
+    if (arg_count > 2) {
+        std::cout << "Usage: " << *args << " [file to read]" << std::endl;
+        return 1;
+    }
 
     executor e;
 
-    // TODO: Take command line args
-    std::string input;
-    while (std::getline(std::cin, input)) {
+    auto execute = [&e](std::string && input) {
         parser p{std::move(input)};
-        input.clear();
 
         auto command_list = p.parse();
-        std::cout << "Parsed commands:\n";
-        for (auto & command : command_list) { std::cout << *command << std::endl; }
+
+        if constexpr (debug) {
+            std::cout << "Parsed commands:\n";
+            for (auto & command : command_list) { std::cout << *command << std::endl; }
+        }
 
         for (auto & command : command_list) command->execute(e);
+    };
+
+    if (arg_count == 2) {
+        execute(read_entire_file(std::ifstream{args[1]}));
+    } else {
+        std::string input;
+        while (std::getline(std::cin, input)) {
+            execute(std::move(input));
+            input.clear();
+        }
     }
 }
